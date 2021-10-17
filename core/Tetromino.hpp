8c9240ac66be57c1725f33d347f489c8075c1737
Cyprien Borée <boreec@tuta.io>
2021-10-17 20:21:49 +0200

    add comments in Tetromino class

--- core/Tetromino.hpp
+++ core/Tetromino.hpp
@@ -6,19 +6,54 @@
 
 namespace Tetris{
     namespace core{
+        /**
+         * @brief Tetromino represents a basic Tetris piece.
+         */
         class Tetromino{
         public:
             Tetromino();
+
+            /**
+             * @brief getChar
+             * @return the char corresponding the piece.
+             */
             virtual char getChar() const = 0;
+
+            /**
+             * @brief getPiece returns the matrix representation of the piece.
+             * @param orientation is the matrix orientation.
+             * @return the matrix of the piece.
+             */
             virtual std::array<std::array<char,4>,4> getPiece(int orientation) const = 0;
+
+            /**
+             * @brief setX defines the new position on the x axis.
+             * @param x is the new position.
+             */
             void setX(const int x);
 
+            /**
+             * @brief setY defines the new position on the y axis.
+             * @param y is the new position.
+             */
             void setY(const int y);
 
+            /**
+             * @brief getX
+             * @return the position on the x axis.
+             */
             int getX() const;
 
+            /**
+             * @brief getY
+             * @return the position on the y axis.
+             */
             int getY() const;
 
+            /**
+             * @brief getOrientation
+             * @return the orientation state.
+             */
             int getOrientation() const;
 
             std::array<std::array<char,4>,4> getPiece() const;
@@ -26,10 +61,19 @@ namespace Tetris{
             void setOrientation(const int);
         protected:
 
+            /**
+             * @brief m_x The piece's position on the x axis.
+             */
             int m_x;
 
+            /**
+             * @brief m_y The piece's position on the y axis.
+             */
             int m_y;
 
+            /**
+             * @brief m_orientation The piece orientation state.
+             */
             int m_orientation;
         };
     }

b995c30a809204d9e43b90b1ab73c3f93e0af5bf
Cyprien Borée <boreec@tuta.io>
2021-10-21 16:08:23 +0200

    add some comments for RendererFacilities

--- gui/RendererFacilities.hpp
+++ gui/RendererFacilities.hpp
@@ -11,9 +11,22 @@ namespace Tetris{
          */
         class RendererFacilities{
         public:
-            RendererFacilities()=delete;
 
-            static void drawBlock(QPainter&, const int, const int, const int, const QColor&);
+            /**
+             * @brief RendererFacilities's constructor is inexistant
+             * because this is a static class only.
+             */
+            RendererFacilities() = delete;
+
+            /**
+             * @brief drawBlock draws a colourfull block according to the provided parameters.
+             * @param painter is the surface to draw on.
+             * @param x is the first coordinate on the x axis.
+             * @param y is the first coordinate on the y axis.
+             * @param size is the size used for the height and width of the block.
+             * @param colour is the colour of the block.
+             */
+            static void drawBlock(QPainter& painter, const int x, const int y, const int size, const QColor& colour);
         };
     }
 }

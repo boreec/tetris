29d5e969861ed7f1ddc93bf59154f5ae220c2904
Cyprien Borée <boreec@tuta.io>
2021-10-24 21:04:13 +0200

    #3 add data members for Tetris::gui::Frame

--- gui/Frame.cpp
+++ gui/Frame.cpp
@@ -1,6 +1,9 @@
 #include "Frame.hpp"
 
-Tetris::gui::Frame::Frame()
+Tetris::gui::Frame::Frame(const QPainterPath& painterPath, const unsigned frameInterval, const QColor& color):
+    m_painterPath(painterPath),
+    m_frameInterval(frameInterval),
+    m_color(color)
 {
 
 }
--- gui/Frame.hpp
+++ gui/Frame.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <QColor>
+#include <QPainterPath>
+
 namespace Tetris::gui{
-    class Frame
-    {
+    class Frame {
     public:
-        Frame();
+        Frame(const QPainterPath& painterPath, const unsigned timeInterval = 0, const QColor& color = Qt::black);
+    protected:
+        unsigned m_frameInterval;
+        QColor m_color;
+        QPainterPath m_painterPath;
     };
 }
 

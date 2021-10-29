2709d746b475d7fe013a3d5b8a218be47b33f338
Cyprien Borée <boreec@tuta.io>
2021-10-29 23:00:54 +0200

    #5 update Gui/RendererPreview documentation

--- gui/RendererPreview.hpp
+++ gui/RendererPreview.hpp
@@ -15,27 +15,43 @@ namespace Tetris::gui{
      */
     class RendererPreview : public QOpenGLWidget{
     public:
+
+        /**
+         * @brief RendererPreview's constructor
+         * @param tetromino is the Tetromino piece to display.
+         */
         RendererPreview(Tetris::core::Tetromino* tetromino = nullptr);
 
-        /*
-         * Called once before paintGL() or resizeGL().
-         * Set up any required OpenGL resources and state.
-         * */
+        /**
+         * @brief initializeGL is called once before PaintGL or resizeGL.
+         * It sets up any required OpenGL resources and state.
+         */
         void initializeGL() override;
 
-        /*
-         * Renders the OpenGL scene. Gets called whenever
-         * the widget needs to be updated.
-         * */
+        /**
+         * @brief paintGL renders the OpenGL scene.
+         * It is called whenever the widget needs to be updated.
+         */
         void paintGL() override;
 
-        /*
-         * Called whenever the widget has been resized.
-         * */
+        /**
+         * @brief resizeGL is called whenever the widget has been resized.
+         * @param w is the window's width.
+         * @param h is the window's height.
+         */
         void resizeGL(int w, int h) override;
 
+        /**
+         * @brief setTetromino defines the piece to display.
+         * @param tetromino is a pointer to the next piece.
+         */
         void setTetromino(Tetris::core::Tetromino* tetromino);
+
     private:
+
+        /**
+         * @brief m_tetromino is a handle to the Tetromino's piece to display.
+         */
         Tetris::core::Tetromino* m_tetromino;
     };
 }

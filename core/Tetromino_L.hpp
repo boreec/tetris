0f8481d53676c5e80003168dfe55e5751dafc237
Cyprien Borée <boreec@tuta.io>
2021-10-30 23:10:24 +0200

    #5 update Tetromino_L doc

--- core/Tetromino_L.hpp
+++ core/Tetromino_L.hpp
@@ -5,10 +5,28 @@
 #define L_CHAR 'L'
 
 namespace Tetris::core{
+
+    /**
+    * @brief The Tetromino_L represents the L Tetromino piece.
+    * This piece is the mirror of Tetromino_J.
+    */
     class Tetromino_L : public Tetris::core::Tetromino {
     public:
+
+        /**
+         * @brief Tetromino_L's constructor.
+         */
         Tetromino_L();
+
+        /**
+         * @brief getChar returns the char corresponding to a cell occupied by a O piece.
+         */
         char getChar() const override;
+
+        /**
+         * @brief getPiece returns the piece's matrix as 2d array of characters.
+         * @param orientation specify the matrix's orientation.
+         */
         std::array<std::array<char,4>,4> getPiece(int orientation) const override;
     };
 }

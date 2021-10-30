64961ec3b63875c2ebd4d64e03cd7a97a5441165
Cyprien Borée <boreec@tuta.io>
2021-10-30 23:07:36 +0200

    #5 update Tetromino_J piece

--- core/Tetromino_J.hpp
+++ core/Tetromino_J.hpp
@@ -5,10 +5,28 @@
 #define J_CHAR 'J'
 
 namespace Tetris::core{
+
+    /**
+    * @brief The Tetromino_J represents the J Tetromino piece.
+    * This piece is the mirror of Tetromino_L.
+    */
     class Tetromino_J : public Tetris::core::Tetromino{
     public:
+
+        /**
+         * @brief Tetromino_J's constructor.
+         */
         Tetromino_J();
+
+        /**
+         * @brief getChar returns the char corresponding to a cell occupied by a J piece.
+         */
         char getChar() const override;
+
+        /**
+         * @brief getPiece returns the piece's matrix as 2d array of characters.
+         * @param orientation specify the matrix's orientation.
+         */
         std::array<std::array<char,4>,4> getPiece(int orientation) const override;
     };
 }

7aa3fd04aea3d4721df5d458008fa785912051b6
Cyprien Borée <boreec@tuta.io>
2021-10-30 22:42:15 +0200

    reindent Tetromino_S class

--- core/Tetromino_S.hpp
+++ core/Tetromino_S.hpp
@@ -5,11 +5,11 @@
 #define S_CHAR 'S'
 
 namespace Tetris::core{
-        class Tetromino_S : public Tetris::core::Tetromino{
-        public:
-            Tetromino_S();
-            char getChar() const override;
-            std::array<std::array<char,4>,4> getPiece(int orientation) const override;
-        };
+    class Tetromino_S : public Tetris::core::Tetromino{
+    public:
+        Tetromino_S();
+        char getChar() const override;
+        std::array<std::array<char,4>,4> getPiece(int orientation) const override;
+    };
 }
 

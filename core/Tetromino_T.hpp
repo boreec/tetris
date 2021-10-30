b84185ec0fab5a4d5f8c71dd170c2527682e2cf8
Cyprien Borée <boreec@tuta.io>
2021-10-30 23:14:52 +0200

    #5 update Tetromino_T doc

--- core/Tetromino_T.hpp
+++ core/Tetromino_T.hpp
@@ -5,10 +5,27 @@
 #define T_CHAR 'T'
 
 namespace Tetris::core{
+
+    /**
+     * @brief Tetromino_T represents the T Tetromino piece.
+     */
     class Tetromino_T : public Tetris::core::Tetromino{
     public:
+
+        /**
+         * @brief Tetromino_T's constructor.
+         */
         Tetromino_T();
+
+        /**
+         * @brief getChar returns the char corresponding to a cell occupied by a T piece.
+         */
         char getChar() const override;
+
+        /**
+         * @brief getPiece returns the piece's matrix as 2d array of characters.
+         * @param orientation specify the matrix's orientation.
+         */
         std::array<std::array<char,4>,4> getPiece(int orientation) const override;
     };
 }
